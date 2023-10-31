a2cc66ed3ab62576af2f8d69586444927d358dae
Miguel Ángel Lombana García <84014018+miguellombana@users.noreply.github.com>
2023-10-31 20:54:12 +0000

    V1.0.0

--- src/main/java/dad/login/App.java
+++ src/main/java/dad/login/App.java
@@ -1,5 +1,23 @@
 package dad.login;
 
-public class App {
+import dad.mvc.loginController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class App extends Application {
+
+	private loginController control = new loginController();
+
+	@Override
+
+	public void start(Stage primaryStage) throws Exception {
+
+		Scene scene = new Scene(control.getVista(), 320, 200);
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Login modelo vista controlador");
+		primaryStage.show();
+
+	}
 }
--- src/main/java/dad/login/Main.java
+++ src/main/java/dad/login/Main.java
@@ -1,30 +1,13 @@
 package dad.login;
 
-import dad.login.auth.AuthService;
-import dad.login.auth.FileAuthService;
-import dad.login.auth.LdapAuthService;
-import dad.mvc.vista;
 import javafx.application.Application;
 
 public class Main {
-	
-	public static void main(String[] args) throws Exception {
-		
-		
-		
-		Application.launch(vista.class, args);
 
-		
-		
-		boolean useLdap = true;
-		
-		AuthService auth = useLdap ? new LdapAuthService() : new FileAuthService();
-		System.out.println("Chuck Norris    : " + auth.login("cnorris", "patada"));
-		System.out.println("Charles Bronson : " + auth.login("cbronson", "Metrallet@1"));
-		System.out.println("David Carradine : " + auth.login("dcarradine", "kungfu"));
-		System.out.println("David Carradine : " + auth.login("dcarradine", "karate"));
-		System.out.println("Michael Knight  : " + auth.login("mknight", "kit"));
-		
+	public static void main(String[] args) {
+
+		Application.launch(App.class, args);
+
 	}
 
 }
